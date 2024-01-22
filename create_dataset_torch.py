337fe2df3668065ffed2e3bf7496b7f5600a384a
breadbread1984 <breadbread1984@163.com>
2024-01-22 21:19:58 +0800

    update

--- create_dataset_torch.py
+++ create_dataset_torch.py
@@ -62,7 +62,7 @@ class RhoDataset(Dataset):
     x, y = data['x'].astype(np.float32), np.expand_dims(data['y'], axis = -1).astype(np.float32)
     value = np.maximum(1e-32 * np.ones_like(x), np.abs(x))
     x = np.where(x >= 0, value, -value)
-    return 1/x, y
+    return 1/x, np.exp(y)
 
 if __name__ == "__main__":
   add_options()
