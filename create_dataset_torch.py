99654f10732145c4934f77a81e6b3880a5280fdf
breadbread1984 <breadbread1984@163.com>
2024-01-22 11:13:22 +0800

    update

--- create_dataset_torch.py
+++ create_dataset_torch.py
@@ -8,21 +8,18 @@ from torch.utils.data import Dataset, DataLoader
 class RhoDataset(Dataset):
   def __init__(self, dataset_dir, eval_dists, train = True):
     super(RhoDataset, self).__init__()
-    self.dataset_dir = dataset_dir
-    self.eval_dists = eval_dists
-    self.is_train = is_train
     self.file_list = list()
     self.sample_count = 0
-    for molecule in listdir(self.dataset_dir):
-      if not isdir(join(self.dataset_dir, molecule)): continue
-      for bond in listdir(join(self.dataset_dir, molecule)):
+    for molecule in listdir(dataset_dir):
+      if not isdir(join(dataset_dir, molecule)): continue
+      for bond in listdir(join(dataset_dir, molecule)):
         stem, ext = splitext(bond)
         if ext != '.npy': continue
         distance = int(stem.replace('data_', ''))
         is_train_sample = True if distance not in eval_dists else False
         if train == is_train_sample:
-          data = np.load(join(self.dataset_dir, molecule, bond))
-          self.sample_list((join(self.dataset_dir, molecule, bond), self.sample_count, self.sample_count + data.shape[0]))
+          data = np.load(join(dataset_dir, molecule, bond))
+          self.sample_list((join(dataset_dir, molecule, bond), self.sample_count, self.sample_count + data.shape[0]))
           self.sample_count += data.shape[0]
   def __len__(self):
     return self.sample_count
