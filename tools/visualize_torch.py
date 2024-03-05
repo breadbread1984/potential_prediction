3b73cf5f3bb1444364f48f6e1906fc406a37c13b
breadbread1984 <breadbread1984@163.com>
2024-03-05 08:59:25 +0800

    update

--- tools/visualize_torch.py
+++ tools/visualize_torch.py
@@ -77,7 +77,7 @@ def main(unused_argv):
     plt.savefig('%s_mae.png' % molecule)
     with open('%s_mae.csv' % molecule, 'w') as f:
       f.write('bond distance, MAE\n')
-      with result in maes:
+      for result in maes:
         f.write("%f,%f\n" % (result[0],result[1]))
 
 if __name__ == "__main__":
