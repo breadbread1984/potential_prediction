56d0d8aef9fcb4342e33ad70b1d038aa6a0017f4
breadbread1984 <breadbread1984@163.com>
2024-03-22 16:56:42 +0800

    update

--- create_dataset_torch.py
+++ create_dataset_torch.py
@@ -5,18 +5,14 @@ import numpy as np
 from torch.utils.data import Dataset
 
 class RhoDataset(Dataset):
-  def __init__(self, dataset_dir, divide = 'train'):
-    assert divide in {'train', 'eval'}
+  def __init__(self, npy_path):
     super(RhoDataset, self).__init__()
-    file_name = {'train': 'alldata.npy', 'eval': 'rho_all.npy'}[divide]
-    self.samples = np.load(join(dataset_dir, file_name))
-    self.labels = np.load(join(dataset_dir, 'vxc_all.npy'))
-    assert self.samples.shape[0] == self.labels.shape[0]
+    self.samples = np.load(npy_path)
   def __len__(self):
     return self.samples.shape[0]
   def __getitem__(self, idx):
-    x = np.reshape(self.samples[idx,3:], (1,11,11,11)).astype(np.float32) # x.shape = (1,11,11,11)
-    y = np.expand_dims(self.labels[idx], axis = -1).astype(np.float32) # y.shape = (1)
-    label = np.arcsinh(y)
-    return x, label
+    x = np.reshape(self.samples[idx,:-1], (1,11**3+31)).astype(np.float32) # x.shape = (1,1362)
+    y = self.samples[idx,-1:].astype(np.float32) # y.shape = (1)
+    x = np.log10(x)
+    return x, y
 
